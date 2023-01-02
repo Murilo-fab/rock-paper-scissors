f224c1c94f3248792769d99fab6e08eaed7a8cf4
Murilo <murilox26@hotmail.com.br>
2023-01-02 15:21:02 -0300

    Added an UI

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
+/* Variables */
+let options = ['rock', 'paper', 'scissors']
+let playerScore = 0;
+let computerScore = 0;
+
+/* Nodes */
+let buttons = document.querySelectorAll("button");
+let round = document.querySelector(".round");
+let score = document.querySelector(".score")
+let result = document.querySelector(".result")
+
 function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)]
 }
 
 function playRound (playerSelection, computerChoice) {
-    playerSelection = playerSelection.toLowerCase();
 
-    if ( (playerSelection == 'rock' && computerChoice == 'scissors') || (playerSelection == 'scissors' && computerChoice == 'paper') || (playerSelection == 'paper' && computerChoice == 'rock')) {
-        return "You Win!!!"
-    } else if ( playerSelection == computerChoice) {
-        return "Tie!!!"
-    } else {
-        return "You Lose!!!"
-    }
+    if ( (playerSelection == 'rock' && computerChoice == 'scissors') || 
+        (playerSelection == 'scissors' && computerChoice == 'paper') || 
+        (playerSelection == 'paper' && computerChoice == 'rock')){
+        return "player"
+    } else if ( playerSelection == computerChoice) return "Tie!!!";
+    else return "computer";
 }
 
-function game() {
+function main () {    
+        
+    buttons.forEach((button) => {
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock, Paper or Scissors? ")
-        computerChoice = getComputerChoice()
+        function play () {
 
-        console.log(playRound(playerSelection, computerChoice))
-    }
+            if (playerScore < 5 && computerScore < 5) {
+
+                playerSelection = button.id;
+                computerChoice = getComputerChoice();
+
+                round.textContent = "The computer chose " + computerChoice;
+            
+                match = playRound(playerSelection, computerChoice)
+                    
+                if (match == "player")
+                    playerScore++;
+                else if (match == "computer")
+                    computerScore++;
+                    
+                score.textContent = "Player " + playerScore + " X " + computerScore + " Computer ";
+        
+                if (playerScore == 5) 
+                    result.textContent = "Player won!!!";
+                else if (computerScore == 5)
+                    result.textContent = "Computer won!!!";
+                
+            } else {
+                button.removeEventListener("click", play);
+            }
+        }
+    
+        button.addEventListener('click', play)
+    })
+
 }
 
-game()
+main()
